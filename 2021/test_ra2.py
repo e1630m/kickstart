52a5e728bc414457264d66dd212e50156db5eace
e1630m <e1630m@gmail.com>
2021-10-06 00:47:09 +0000

    Update test_ra2.py, introducing new test method

--- 2021/test_ra2.py
+++ 2021/test_ra2.py
@@ -1,50 +1,42 @@
+from contextlib import redirect_stdout
+from io import StringIO
 from time import perf_counter_ns
 from unittest import TestCase
-from ra2 import *
+from unittest.mock import MagicMock, patch
 import os
+from ra2 import *
 
 
 class Tester(TestCase):
-    def set_up(self):
+    def test(self, opt='silent'):
         n = os.path.basename(__file__).replace('.py', '').replace('test_', '')
         p = os.path.join(os.path.dirname(__file__), 'data\\').replace('\\', '/')
         i = [p + f for f in os.listdir('data') if n in f and 'inp' in f]
         o = [p + f for f in os.listdir('data') if n in f and 'out' in f]
-        data = []
         for ts_no in range(len(i)):
             with open(i[ts_no], 'r') as f:
-                ti = f.readlines()
+                inputs = [line.strip('\n') for line in f.readlines()]
             with open(o[ts_no], 'r') as f:
-                to = f.readlines()
-            num_tests = int(ti.pop(0))
-            grids = {}
-            for t_no in range(num_tests):
-                r, c = map(int, ti.pop(0).strip('\n').split())
-                tmp = []
-                for _ in range(r):
-                    row = ti.pop(0)
-                    tmp.append([int(n) for n in row.split()])
-                grids[t_no + 1] = {'grid': tmp, 'expected': to.pop(0).strip('\n')}
-            data.append(grids)
-        self.data = data
-
-    def test_func(self, opt='silent'):
-        self.set_up()
-        for no, test_set in enumerate(self.data):
-            start = perf_counter_ns()
-            for i in range(len(test_set)):
-                t = test_set[i + 1]
-                grid = t['grid'],
-                e = int(t['expected'][t['expected'].index(':') + 2:])
-                actual = funcname(grid)
-                self.assertEqual(e, actual)
-                if opt == 'verbose':
-                    print(f'Test Set {no + 1}: Test {i + 1} Passed')
+                outputs = [line.strip('\n') for line in f.readlines()]
+            m = MagicMock()
+            f = StringIO()
+            length = len(inputs)
+            with redirect_stdout(f):
+                with patch('builtins.input', side_effect=inputs):
+                    reader()
+                    start = perf_counter_ns()
+                    for _ in range(length):
+                        m()
             end = perf_counter_ns()
-            print(f'Test Set {no + 1}: {len(test_set)} tests passed '
+            result = f.getvalue().split('\n')[:-1]
+            for case_no, (actual, expected) in enumerate(zip(result, outputs)):
+                self.assertEqual(actual, expected)
+                if opt == 'verbose':
+                    print(f'Test Set {ts_no + 1}: Test {case_no + 1} Passed')
+            print(f'Test Set {ts_no + 1}: {len(outputs)} tests passed '
                   f'in {end - start:,}ns')
 
 
 if __name__ == '__main__':
     tester = Tester()
-    tester.test_func()
+    tester.test()
