18b29a0a427ff12fed49272df316e06066a5eb3a
e1630m <e1630m@gmail.com>
2021-10-07 04:13:48 +0000

    Add test_ra4

---
+++ 2021/test_ra4.py
@@ -0,0 +1,40 @@
+from contextlib import redirect_stdout
+from io import StringIO
+from time import perf_counter_ns
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+import os
+from ra4 import *
+
+
+class Tester(TestCase):
+    def test(self):
+        n = os.path.basename(__file__).replace('.py', '').replace('test_', '')
+        p = os.path.join(os.path.dirname(__file__), 'data/')
+        ipath = sorted(p + f for f in os.listdir(p) if n in f and 'inp' in f)
+        opath = sorted(p + f for f in os.listdir(p) if n in f and 'out' in f)
+        for ts_no in range(len(ipath)):
+            with open(ipath[ts_no], 'r') as f:
+                inputs = [line.strip('\n') for line in f.readlines()]
+            with open(opath[ts_no], 'r') as f:
+                outputs = [line.strip('\n') for line in f.readlines()]
+            m = MagicMock()
+            f = StringIO()
+            length = len(inputs)
+            start = perf_counter_ns()
+            with redirect_stdout(f):
+                with patch('builtins.input', side_effect=inputs):
+                    reader()
+                    for _ in range(length):
+                        m()
+            result = f.getvalue().split('\n')[:-1]
+            for case_no, (actual, expected) in enumerate(zip(result, outputs)):
+                self.assertEqual(actual, expected)
+            end = perf_counter_ns()
+            print(f'Test Set {ts_no + 1}: {len(outputs)} tests passed '
+                  f'in {(end - start) // int(1e6):,}ms')
+
+
+if __name__ == '__main__':
+    tester = Tester()
+    tester.test()
